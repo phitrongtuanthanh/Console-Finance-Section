19bef03016f55dd42b4dca21b474f65ff5d88009
phitrongtuanthanh <phitrongtuanthanh@gmail.com>
2022-11-15 11:17:50 +0000

    fix-JS

--- JS file/Tim_file.js
+++ JS file/Tim_file.js
@@ -2,42 +2,70 @@
 var monthNumber = 0;
 var sum = 0;
 var change = 0;
-var previousMonth = 0;
 var changeTotal = 0;
 var average = 0;
-
+var GIncrease = -Infinity;
+var GIMonth = '';
+var GDecrease = Infinity;
+var GDMonth = '';
 
 //Loop inference
+//Intro
 console.log('Financial Analysis');
 console.log('----------------');
 
+//Total months calculation
 for (var index = 0; index < finances.length; index++) {
     monthNumber++;
     }
-if (!(index<finances.length)) {
-    console.log('Total months: ' + monthNumber);
-    }
 
-for (var index = 0; index<finances.length; index ++) {
+console.log(`Total Months: ${monthNumber}`);
+    
+//Total rev calculation
+for (var index = 0; index<finances.length; index++) {
     sum = sum + finances[index][1];
     }
-if(!(index<finances.length)) {
-    console.log('Total: ' + sum);
-    }
+
+console.log(`Total: $(${sum})`);
+
+//Average change calculation
+var previousMonth = 0;
 
 for (var index = 0; index < finances.length; index++) {
     change = finances[index][1] - previousMonth;
     previousMonth = finances[index][1];
     changeTotal = changeTotal + change;
     }
-if (!(index<finances.length)) {
-    average = changeTotal / finances.length;
-    console.log ('total average change: '+ average);
-    }
 
-    for (var index = 0; index < finances.length; index++) {
-        var change = finances.map(x => finances[index++][1] - finances[index]);
-        }
-    if (!(index<finances.length)) {
-        console.log (change);
-        }
+average = changeTotal / finances.length;
+console.log (`Average Change: $(${average})`);
+
+
+// The greatest increase in profits calculation
+var previousMonth = 0;
+
+for (index = 0; index < finances.length; index++) {
+  var currentChange = finances[index][1] - previousMonth;
+  previousMonth = finances[index][1];
+  if (GIncrease < currentChange) {
+    GIncrease = currentChange;
+    GIMonth = finances[index][0];
+    }
+}
+
+console.log(`Greatest Increase in Profits: ${GIMonth} $(${GIncrease})`);
+
+// The greatest decrease in profits calculation
+
+var previousMonth = 0;
+
+for (index = 0; index < finances.length; index++) {
+    var currentChange = finances[index][1] - previousMonth;
+    previousMonth = finances[index][1];
+    if (GDecrease > currentChange) {
+      GDecrease = currentChange;
+      GDMonth = finances[index][0];
+      }
+  }
+  
+  console.log(`Greatest Increase in Profits: ${GDMonth} $(${GDecrease})`);
